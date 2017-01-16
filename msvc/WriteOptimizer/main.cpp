f3cda2592aa5956ca9b85d3e170555d573a60c3c
baileys-am <scantrell7accts@gmail.com>
2017-01-15 21:54:47 -0500

    Added trials and fixed speed calculation

--- msvc/WriteOptimizer/main.cpp
+++ msvc/WriteOptimizer/main.cpp
@@ -7,47 +7,57 @@
 
 int main()
 {
-	std::vector<size_t> bufferSizes = { 256, 512, 1024, 2048, 4096 };
-	bufferSizes.resize(5);
-	std::vector<long long> totalTimes;
-	std::vector<char> buffer;
+    size_t numberOfTrials = 1000;
+    std::vector<size_t> bufferSizes = { 256, 512, 1024, 2048, 4096, 8192, 16384, 32678 };
+    std::vector<long long> totalTimes(bufferSizes.size());
+    std::vector<long long> trialTimes(numberOfTrials);
+    std::vector<char> buffer;
 
-	for (size_t i = 0; i < bufferSizes.size(); i++)
-	{
-		// Initialize buffer
-		buffer.resize(bufferSizes[i]);
-		std::iota(std::begin(buffer), std::end(buffer), 0);
+    for (size_t i = 0; i < bufferSizes.size(); i++)
+    {
+        // Initialize buffers
+        buffer.resize(bufferSizes[i]);
+        std::iota(std::begin(buffer), std::end(buffer), 0);
 
-		// Open file
-		FILE* filePointer;
-		filePointer = fopen("outfile.bin", "wb");
+        // Caculate write properties
+        size_t elementSize = sizeof(buffer.front());
+        size_t elementCount = buffer.size();
 
-		// Caculate write properties
-		size_t elementSize = sizeof(buffer.front());
-		size_t elementCount = buffer.size();
+        for (size_t j = 0; j < numberOfTrials; j++)
+        {
+            // Open file
+            FILE* filePointer;
+            filePointer = fopen("outfile.bin", "wb");
 
-		// Get start time
-		auto startTime = std::chrono::high_resolution_clock::now();
+            // Get start time
+            auto startTime = std::chrono::high_resolution_clock::now();
 
-		// Perform write test
-		fwrite(&buffer.front(), elementSize, elementCount, filePointer);
-		
-		// Get end time
-		auto endTime = std::chrono::high_resolution_clock::now();
+            // Perform write test
+            fwrite(&buffer.front(), elementSize, elementCount, filePointer);
 
-		// Close file
-		fclose(filePointer);
+            // Get end time
+            auto endTime = std::chrono::high_resolution_clock::now();
 
-		// Calcualte total time
-		auto totalTime = std::chrono::duration_cast<std::chrono::nanoseconds>(endTime - startTime).count();
+            // Close file
+            fclose(filePointer);
 
-		// Save total time
-		totalTimes.push_back(totalTime);
+            // Calcualte total time
+            auto totalTime = std::chrono::duration_cast<std::chrono::nanoseconds>(endTime - startTime).count();
 
-		// Output run info
-		std::cout << "Write speed of " << (1e9 * bufferSizes[i]) / (1e9 * totalTime) << " B/s using buffer size of " << bufferSizes[i] << std::endl;
-	}
+            // Save trial time
+            trialTimes[j] = totalTime;
+        }
 
-	getchar();
-	return 0;
+        // Average trials
+        float averageTime = std::accumulate(trialTimes.begin(), trialTimes.end(), 0.0) / trialTimes.size();
+
+        // Save total time
+		totalTimes[i] = averageTime;
+
+        // Output run info
+        std::cout << "Write speed of " << ((float)bufferSizes[i] / 1e6) / (averageTime * 1e-9) << " MB/s using buffer size of " << bufferSizes[i] << std::endl;
+    }
+
+    getchar();
+    return 0;
 }
